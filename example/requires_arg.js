91f53a1aeedb5959a802cf81a9ddae962418d11c
shreyasch <56544074+shreyasch@users.noreply.github.com>
2022-04-13 04:29:13 -0400

    added type

--- example/requires_arg.js
+++ example/requires_arg.js
@@ -3,11 +3,13 @@ var yargs = require('yargs/yargs')(process.argv.slice(2));
 
 var argv = yargs.usage('This is my awesome program').options({
   input: {
+    type: "string",
     description: 'Input file name',
     requiresArg: true,
     alias: 'i',
   },
   output: {
+    type: "string",
     description: 'Output file name',
     requiresArg: true,
     alias: 'o',

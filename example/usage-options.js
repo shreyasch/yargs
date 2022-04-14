5db63028fb894b2ee90777c1f79aae2c1e627c49
shreyasch <56544074+shreyasch@users.noreply.github.com>
2022-04-14 20:19:45 +0530

    Update usage-options.js

--- example/usage-options.js
+++ example/usage-options.js
@@ -4,11 +4,13 @@ var yargs = require('yargs/yargs')(process.argv.slice(2));
 var argv = yargs.usage('This is my awesome program')
   .options({
     'about': {
+      type: 'string',
       description: 'Provide some details about the author of this program',
       required: true,
       alias: 'a',
     },
     'info': {
+      type: 'string',
       description: 'Provide some information about the node.js agains!!!!!!',
       boolean: true,
       alias: 'i'
